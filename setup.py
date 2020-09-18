a5cc82d708af45759977db011158ce9828ab5cc3
Kalle Westerling <KWesterling1@gradcenter.cuny.edu>
2020-09-18 12:33:05 -0400

    changing workshop_title - important!

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from localdjango.setup import read, check_sections_directory, split_into_section
 
 if __name__ == "__main__":
 
-    workshop_title = 'test'
+    workshop_title = 'Python'
 
     # setup 'sections' directory
     check_sections_directory()
